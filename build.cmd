e07faa9f1df21bc8cf82eed8edafb9cf11144512
nil4 <nil4@users.noreply.github.com>
2018-06-01 00:13:34 +0200

    Add .NET 4.6.1 standalone executable.

--- build.cmd
+++ build.cmd
@@ -1,7 +1,20 @@
 @echo off
+setlocal
 
-dotnet.exe msbuild /t:Restore /p:Configuration=Release;Pack=lib
-dotnet.exe msbuild /t:Pack    /p:Configuration=Release;Pack=lib
+dotnet.exe msbuild dotnet-xdt /t:Restore /p:Configuration=Release /p:As=lib
+dotnet.exe msbuild dotnet-xdt /t:Pack    /p:Configuration=Release /p:As=lib
 
-dotnet.exe msbuild /t:Restore /p:Configuration=Release;Pack=tool
-dotnet.exe msbuild /t:Pack    /p:Configuration=Release;Pack=tool
+dotnet.exe msbuild dotnet-xdt /t:Restore /p:Configuration=Release /p:As=tool
+dotnet.exe msbuild dotnet-xdt /t:Pack    /p:Configuration=Release /p:As=tool
+
+dotnet.exe msbuild dotnet-xdt /t:Restore /p:Configuration=Release /p:As=exe
+dotnet.exe msbuild dotnet-xdt /t:Build   /p:Configuration=Release /p:As=exe
+
+dotnet.exe msbuild /t:Restore /p:Configuration=Release
+
+pushd
+cd dotnet-xdt.tests
+dotnet.exe fixie --configuration Release
+popd
+
+

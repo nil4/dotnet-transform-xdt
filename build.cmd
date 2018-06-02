2046d2b3365e8bb892a64c580d0f4df0931fd867
nil4 <nil4@users.noreply.github.com>
2018-06-02 17:20:35 +0200

    Extract build and test to batch files

--- build.cmd
+++ build.cmd
@@ -1,5 +1,4 @@
 @echo off
-setlocal
 
 dotnet.exe msbuild dotnet-xdt /t:Restore /p:Configuration=Release /p:As=lib
 dotnet.exe msbuild dotnet-xdt /t:Pack    /p:Configuration=Release /p:As=lib
@@ -9,10 +8,3 @@ dotnet.exe msbuild dotnet-xdt /t:Pack    /p:Configuration=Release /p:As=tool
 
 dotnet.exe msbuild dotnet-xdt /t:Restore /p:Configuration=Release /p:As=exe
 dotnet.exe msbuild dotnet-xdt /t:Build   /p:Configuration=Release /p:As=exe
-
-dotnet.exe msbuild /t:Restore /p:Configuration=Release
-
-pushd
-cd dotnet-xdt.tests
-dotnet.exe fixie --configuration Release
-popd

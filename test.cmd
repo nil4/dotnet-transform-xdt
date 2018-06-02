9f45d8b872acd04749b4a414f3a56a5453cbf4e8
nil4 <nil4@users.noreply.github.com>
2018-06-02 17:27:15 +0200

    Fix pushd/popd in test.cmd

--- test.cmd
+++ test.cmd
@@ -2,7 +2,6 @@
 
 dotnet.exe msbuild /t:Restore /p:Configuration=Release
 
-pushd
-cd dotnet-xdt.tests
+pushd dotnet-xdt.tests
 dotnet.exe fixie --configuration Release --report test-results.xml
 popd

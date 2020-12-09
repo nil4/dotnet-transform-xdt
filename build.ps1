a818f62581c4e9fa0187d3f1a4a502a877db6363
nil4 <nil4@users.noreply.github.com>
2020-12-09 11:10:25 +0100

    Fix typo (`ContinuousIntegrationBuild`)

--- build.ps1
+++ build.ps1
@@ -9,11 +9,11 @@ function step($command) {
 $Env:DOTNET_NOLOGO = 'true'
 $Env:DOTNET_CLI_TELEMETRY_OPTOUT = 'true'
 
-step { dotnet msbuild dotnet-xdt /t:Restore /p:Configuration=Release /p:As=lib  /p:ContinuosIntegrationBuild=true }
-step { dotnet msbuild dotnet-xdt /t:Pack    /p:Configuration=Release /p:As=lib  /p:ContinuosIntegrationBuild=true }
+step { dotnet msbuild dotnet-xdt /t:Restore /p:Configuration=Release /p:As=lib  /p:ContinuousIntegrationBuild=true }
+step { dotnet msbuild dotnet-xdt /t:Pack    /p:Configuration=Release /p:As=lib  /p:ContinuousIntegrationBuild=true }
 
-step { dotnet msbuild dotnet-xdt /t:Restore /p:Configuration=Release /p:As=tool /p:ContinuosIntegrationBuild=true }
-step { dotnet msbuild dotnet-xdt /t:Pack    /p:Configuration=Release /p:As=tool /p:ContinuosIntegrationBuild=true }
+step { dotnet msbuild dotnet-xdt /t:Restore /p:Configuration=Release /p:As=tool /p:ContinuousIntegrationBuild=true }
+step { dotnet msbuild dotnet-xdt /t:Pack    /p:Configuration=Release /p:As=tool /p:ContinuousIntegrationBuild=true }
 
-step { dotnet msbuild dotnet-xdt /t:Restore /p:Configuration=Release /p:As=exe  /p:ContinuosIntegrationBuild=true }
-step { dotnet msbuild dotnet-xdt /t:Build   /p:Configuration=Release /p:As=exe  /p:ContinuosIntegrationBuild=true }
+step { dotnet msbuild dotnet-xdt /t:Restore /p:Configuration=Release /p:As=exe  /p:ContinuousIntegrationBuild=true }
+step { dotnet msbuild dotnet-xdt /t:Build   /p:Configuration=Release /p:As=exe  /p:ContinuousIntegrationBuild=true }
